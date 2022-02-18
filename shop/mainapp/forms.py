c558fcbff12a3358d3b7a36dcef5f81ce17d12ab
DzirtH <68776022+DzirtH@users.noreply.github.com>
2022-02-19 01:18:10 +0500

    Add files via upload

---
+++ shop/mainapp/forms.py
@@ -0,0 +1,21 @@
+from attr import attr
+from django import forms
+from importlib_metadata import files
+
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['order_date'].label = 'Дата получения заказа'
+    order_date = forms.DateField(
+        widget=forms.TextInput(attrs={'type': 'date'}))
+
+    class Meta:
+        model = Order
+
+        fields = (
+            'first_name', 'last_name', 'phone', 'address', 'buying_type', 'order_date', 'comment'
+        )
